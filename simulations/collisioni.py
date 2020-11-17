7774e16f1e67d6b97c4c7c12e6d92630c8f94137
Matteo <suffredinimatteo@gmail.com>
2020-11-17 20:53:47 +0100

    Modifiche script generazione grafici, ordinamento delle etichette e rese visibili i valori su asse x

--- simulations/collisioni.py
+++ simulations/collisioni.py
@@ -18,10 +18,30 @@ def extractHeader(rowFile):
         name = name.replace("EpidemicBroadcast.","")
 
         names[name] = rowFile['attrvalue'][i]
+    return names
+
+def orderkey(list):
+    tList = []
+    mList = []
+    # Cerco gli intervalli dei valori t ed m
+    for key in list.keys():
+        splitted = key.split("-")
+        tList.append(int(splitted[0].replace("t:","")))
+        mList.append(int(splitted[1].replace("m:","")))
+
+    minT = min(tList)
+    maxT = max(tList)
+    minM = min(mList)
+    maxM = max(mList)
 
 
-    return names;
-
+    keyOrdered = []
+    # Genero i label ordinati
+    for t in range(minT, maxT+1, 1):
+        for m in range(minM, maxM+1, 1):
+            if m <= t:
+                keyOrdered.append("t:" + str(t) + "-" + "m:" + str(m))
+    return keyOrdered
 
 collisionValues = {}
 receivedPacketsValues = {}
@@ -36,9 +56,8 @@ for file in files:
 
         header = extractHeader(rowFile)
 
-        custom_key = header['m']+':'+header['T']
+        custom_key = "t:"+header['T']+"-"+"m:"+header['m']
 
-        #22 sarebbe simTime
         for i in range(0,len(rowFile['value'])):
             if rowFile['type'][i] != 'scalar':      #Skipping the header
                 continue
@@ -62,45 +81,53 @@ meanReceivedPackets = {}
 for key in collisionValues.keys():
     curr = collisionValues[key]
     meanCollision[key] = np.mean(curr)
-    print(meanCollision[key])
+    #print(meanCollision[key])
 
-for key in receivedPacketsValues.keys():
-    curr = receivedPacketsValues[key]
-    #print(curr)
-    meanReceivedPackets[key] = np.mean(curr)
-    #print(meanReceivedPackets[key])
+
 
 y_values = []
 x_values = []
-
-for key in meanCollision.keys():
+keyOrdered1 = orderkey(meanCollision)
+for key in keyOrdered1:
     y_values.append(meanCollision[key])    
     x_values.append(key)
 
-
+plt.figure(figsize=(20,10))
+plt.xticks(range(len(x_values)), x_values, size='small',rotation=90)
 plt.title('Collision Analysis')
 plt.xlabel('T,M')
 plt.ylabel('Avg Collisions')
-plt.xticks(rotation=90)
-plt.xticks(range(len(x_values)), x_values, size='small') 
+plt.xticks(range(len(x_values)), x_values, size='small')
 #plt.yticks(np.arange(min(y_values), max(y_values)+50, 10.0))
 plt.grid(True)
 plt.scatter(x_values, y_values)
 plt.savefig('graph_collisions.png')
 
+
+
+
+for key in receivedPacketsValues.keys():
+    curr = receivedPacketsValues[key]
+    meanReceivedPackets[key] = np.mean(curr)
+
 y_values = []
 x_values = []
 
-for key in meanReceivedPackets.keys():
-    y_values.append(meanReceivedPackets[key])    
+keyOrdered2 = orderkey(meanReceivedPackets)
+
+for key in keyOrdered2:
+    y_values.append(meanReceivedPackets[key])
     x_values.append(key)
 
+
+plt.figure(figsize=(20,10))
+plt.xticks(range(len(x_values)), x_values, size='small',rotation=90)
 plt.title('Received Packets Analysis')
-plt.xlabel('T,M')
+plt.xlabel('t,m')
 plt.ylabel('Avg Packets Received')
-plt.xticks(rotation=90)
 #plt.xticks(np.arange(min(radius), max(radius)+50, 50.0))
 #plt.yticks(np.arange(min(y_values), max(y_values)+50, 10.0))
 plt.grid(True)
 plt.scatter(x_values, y_values)
 plt.savefig('graph_packets.png')
+
